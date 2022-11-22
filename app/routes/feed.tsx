fea26a0256e3196eda36abdbd10de4cc5b5c7858
mozzey <michael.cacciano@gmail.com>
2022-11-22 02:31:57 -0500

    side nav design started

--- app/routes/feed.tsx
+++ app/routes/feed.tsx
@@ -1,6 +1,7 @@
 import type { LoaderFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
+import { UserIcon, UsersIcon } from '@heroicons/react/20/solid';
 
 import { db } from '~/utils/db.server';
 import { getUser } from '~/utils/session.server';
@@ -29,7 +30,18 @@ export default function FeedPage() {
 
   return (
     <div className="absolute inset-0 flex max-w-screen-xl mx-auto" style={{ paddingTop: '65px' }}>
-      <div className="w-0 sm:w-1/6 md:w-1/4 bg-gray-200"></div>
+      <div className="w-0 sm:w-1/6 md:w-1/4 bg-gray-200">
+        <ul className="flex flex-col items-center">
+          <li className="flex items-center gap-x-4 p-4 ">
+            <UsersIcon width={35} />
+            <p className="flex-1 text-2xl">Parties</p>
+          </li>
+          <li className="flex items-center gap-x-4 p-4 ">
+            <UserIcon width={35} />
+            <p className="flex-1 text-2xl">Users</p>
+          </li>
+        </ul>
+      </div>
       <div className="w-full md:w-1/2 flex-1 overflow-y-scroll border-r border-l border-black">
         <Feed parties={data?.parties} />
       </div>
