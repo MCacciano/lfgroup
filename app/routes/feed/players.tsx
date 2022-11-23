695b8f84f64925030c9220818d5637f6dad8d640
mozzey <michael.cacciano@gmail.com>
2022-11-23 06:10:29 -0500

    side nav built and working with feed routes, created players and parties routes under feed, currently showing all users and parties in the feed

---
+++ app/routes/feed/players.tsx
@@ -0,0 +1,36 @@
+import type { LoaderFunction } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+
+import { db } from '~/utils/db.server';
+
+type FeedLoaderData = {
+  players: {
+    id: string;
+    username: string;
+  }[];
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const players = await db.user.findMany();
+
+  return json({ players });
+};
+
+export default function PlayersFeed() {
+  const data: FeedLoaderData = useLoaderData();
+
+  console.log('data', data);
+
+  return (
+    <div>
+      {data?.players.map(player => {
+        return (
+          <div key={player.id} className="border-b border-black h-44 p-2 hover:bg-gray-100">
+            {player.username}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
